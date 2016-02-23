569a15da7e3e12472f8ef9b6718ae08ffec44af4
gustavovaldes <g.valdes.aracena@gmail.com>
2016-02-22 23:32:54 -0300

    Merge Sort

---
+++ src/main/java/com/guga/algs1p1/week3/MergeSort.java
@@ -0,0 +1,49 @@
+package com.guga.algs1p1.week3;
+
+/**
+ * Created by guga
+ * <p>
+ * Uses at most N lg N compares and 6 N lg N array accesses to sort any array of size N
+ */
+public class MergeSort {
+
+    private static Comparable[] aux;
+
+    public static void sort(Comparable[] a) {
+        aux = new Comparable[a.length];
+        sort(a, 0, a.length - 1);
+    }
+
+    private static void sort(Comparable[] a, int lo, int hi) {
+        int mid = lo + (hi - lo) / 2;
+        if (hi <= lo) return;
+        sort(a, lo, mid);
+        sort(a, mid + 1, hi);
+        merge(a, lo, mid, hi);
+    }
+
+    /**
+     * Merge a[lo..mid] with a[mid+1..hi]
+     */
+    private static void merge(Comparable[] a, int lo, int mid, int hi) {
+        int lowPivot = lo;
+        int midPivot = mid + 1;
+
+        for (int k = lo; k <= hi; k++) { // Copy a[lo..hi] to aux[lo..hi].
+            aux[k] = a[k];
+        }
+
+        for (int k = lo; k <= hi; k++) { // Merge back to a[lo..hi].
+            if (lowPivot > mid) a[k] = aux[midPivot++];
+            else if (midPivot > hi) a[k] = aux[lowPivot++];
+            else if (less(aux[midPivot], aux[lowPivot])) a[k] = aux[midPivot++];
+            else a[k] = aux[lowPivot++];
+        }
+    }
+
+    private static boolean less(Comparable x, Comparable y) {
+        return x.compareTo(y) < 0;
+    }
+
+
+}
---
+++ src/test/java/com/guga/algs1p1/week3/MergeSortTest.java
@@ -0,0 +1,66 @@
+package com.guga.algs1p1.week3;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by guga
+ */
+public class MergeSortTest {
+
+    @Test
+    public void testSort() {
+        Integer[] values = new Integer[]{1, 3, 7, 2, 4, 6, 9};
+        Integer[] result = new Integer[]{1, 2, 3, 4, 6, 7, 9};
+        MergeSort.sort(values);
+        for (int i = 0; i < result.length; i++) {
+            Assert.assertEquals(values[i],result[i]);
+        }
+    }
+
+    @Test
+    public void testEmpty() {
+        Integer[] values = new Integer[0];
+        MergeSort.sort(values);
+        Assert.assertNotNull(values);
+    }
+
+    @Test
+    public void testOneElement() {
+        Integer[] values = new Integer[]{1};
+        MergeSort.sort(values);
+        Assert.assertEquals(1, (int) values[0]);
+    }
+
+    @Test
+    public void testTwoElement() {
+        Integer[] values = new Integer[]{4, 1};
+        MergeSort.sort(values);
+        Assert.assertEquals(1, (int) values[0]);
+        Assert.assertEquals(4, (int) values[1]);
+    }
+
+    @Test
+    public void testDoubleSort() {
+        Integer[] values = new Integer[]{8, 2, 5, 3, 9, 4, 0, 2, -1, -3, 6, 8};
+        Integer[] expected = new Integer[]{-3, -1, 0, 2, 2, 3, 4, 5, 6, 8, 8, 9};
+        MergeSort.sort(values);
+        for (int i = 0; i < expected.length; i++) {
+            Assert.assertEquals(expected[i], values[i]);
+        }
+        MergeSort.sort(values);
+        for (int i = 0; i < expected.length; i++) {
+            Assert.assertEquals(expected[i], values[i]);
+        }
+    }
+
+    @Test
+    public void testAlreadyOrdered() {
+        Integer[] values = new Integer[]{-1, 4, 5, 8, 10, 42};
+        Integer[] expected = new Integer[]{-1, 4, 5, 8, 10, 42};
+        MergeSort.sort(values);
+        for (int i = 0; i < expected.length; i++) {
+            Assert.assertEquals(expected[i], values[i]);
+        }
+    }
+}
