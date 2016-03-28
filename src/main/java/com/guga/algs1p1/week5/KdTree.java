7b058bc41614f43ea0b2c4d7be12bc471d336ef2
Gustavo Valdes <gvaldes@gustavo-nisum.local>
2016-03-28 16:43:46 -0300

    KdTree in progress

--- src/main/java/com/guga/algs1p1/week5/KdTree.java
+++ src/main/java/com/guga/algs1p1/week5/KdTree.java
@@ -2,6 +2,7 @@ package com.guga.algs1p1.week5;
 
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.StdDraw;
 
 import java.util.Set;
 import java.util.TreeSet;
@@ -17,6 +18,9 @@ public class KdTree {
 
     }
 
+    public static void main(String[] args) { // unit testing of the methods (optional)
+    }
+
     public boolean isEmpty() {  // is the set empty?
         return false;
     }
@@ -27,24 +31,49 @@ public class KdTree {
 
     public void insert(Point2D p) { // add the point to the set (if it is not already in the set)
         if (root == null) {
-            root = new Node(p, new RectHV(0, 0, 1, 1));
+            root = new Node(p);
+            RectHV r = new RectHV(0, 0, 1, 1);
+            root.rect = r;
         } else {
             root = insert(root, p, true);
         }
     }
 
-    private Node insert(Node node, Point2D point, boolean verticalSplit) {
+    private Node insert(Node node, Point2D point, boolean compareByX) {
 
-        if (node == null) return new Node(point, null);
+        if (node == null) return new Node(point);
+        if (node.p.equals(point)) return node;
+
         double direction = 0;
-        if (verticalSplit) {
+        if (compareByX) {
             direction = point.x() - node.p.x();
         } else {
             direction = point.y() - node.p.y();
 
         }
-        if (direction < 0) node.lb = insert(node.lb, point, !verticalSplit);
-        else node.rt = insert(node.rt, point, !verticalSplit);
+        RectHV r = null;
+        if (direction < 0) {
+            node.lb = insert(node.lb, point, !compareByX);
+            if (node.lb.rect == null) {
+                if (compareByX) {
+                    r = new RectHV(node.rect.xmin(), node.rect.ymin(), node.p.x(), node.rect.ymax());
+                } else {
+                    r = new RectHV(node.rect.xmin(), node.rect.ymin(), node.rect.xmax(), node.p.y());
+                }
+                node.lb.rect = r;
+            }
+        } else {
+            node.rt = insert(node.rt, point, !compareByX);
+            if (node.rt.rect == null) {
+                if (compareByX) {
+                    r = new RectHV(node.p.x(), node.rect.ymin(), node.rect.xmax(), node.rect.ymax());
+                } else {
+                    r = new RectHV(node.rect.xmin(), node.p.y(), node.rect.xmax(), node.rect.ymax());
+                }
+                node.rt.rect = r;
+            }
+        }
+
         return node;
     }
 
@@ -73,14 +102,25 @@ public class KdTree {
     }
 
     public void draw() { // draw all points to standard draw
-        if (root != null) draw(root);
+        if (root != null) draw(root, true);
     }
 
-    private void draw(Node node) {
+    private void draw(Node node, boolean compareByX) {
         if (node != null) {
+            StdDraw.setPenColor(StdDraw.BLACK);
+            StdDraw.setPenRadius(.01);
             node.p.draw();
-            draw(node.lb);
-            draw(node.rt);
+            if(compareByX){
+                StdDraw.setPenColor(StdDraw.RED);
+                StdDraw.setPenRadius(.01);
+            }else{
+                StdDraw.setPenColor(StdDraw.BLUE);
+                StdDraw.setPenRadius(.01);
+            }
+
+            node.rect.draw();
+            draw(node.lb, !compareByX);
+            draw(node.rt, !compareByX);
         }
     }
 
@@ -93,19 +133,14 @@ public class KdTree {
         return null;
     }
 
-    public static void main(String[] args) { // unit testing of the methods (optional)
-    }
-
-
     private class Node {
         private Point2D p;      // the point
         private RectHV rect;    // the axis-aligned rectangle corresponding to this node
         private Node lb;        // the left/bottom subtree
         private Node rt;        // the right/top subtree
 
-        Node(Point2D p, RectHV r) {
+        Node(Point2D p) {
             this.p = p;
-            this.rect = r;
         }
     }
 
